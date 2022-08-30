fd3c4ffb1fc6c6a5b957be4110e500937f7517a3
UpHash <yukawallstudio@gmail.com>
2022-08-31 06:52:56 +0900

    pj settings (vklib link)

--- VulkanTest/VulkanTest.cpp
+++ VulkanTest/VulkanTest.cpp
@@ -1,11 +1,63 @@
-﻿// VulkanTest.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#pragma once
 
 #include <iostream>
+#include <vector>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan/vk_layer.h>
+#include <vulkan/vulkan_win32.h>
+#pragma comment(lib, "vulkan-1.lib")
+
+void Test()
+{
+    const auto layers = std::vector{ "VK_LAYER_KHRONOS_validation" };
+    uint32_t extentionsCount;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extentionsCount, nullptr);
+    auto extentions = std::vector<VkExtensionProperties>(extentionsCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extentionsCount, extentions.data());
+    auto extentionNames = std::vector<const char*>();
+    for (auto& extention : extentions)
+    {
+        extentionNames.push_back(extention.extensionName);
+    }
+
+    VkApplicationInfo applicationInfo =
+    {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "App",
+        .pEngineName = "App",
+        .engineVersion = VK_MAKE_VERSION(0, 0, 0),
+        .apiVersion = VK_API_VERSION_1_1,
+    };
+    VkInstanceCreateInfo instanceCreateInfo =
+    {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &applicationInfo,
+        .enabledLayerCount = (uint32_t)layers.size(),
+        .ppEnabledLayerNames = layers.data(),
+        .enabledExtensionCount = (uint32_t)extentionNames.size(),
+        .ppEnabledExtensionNames = extentionNames.data(),
+    };
+    VkInstance instance;
+    vkCreateInstance(&instanceCreateInfo, nullptr, &instance);
+
+    uint32_t physicalDeviceCount;
+    vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr);
+    auto physicalDevices = std::vector<VkPhysicalDevice>(physicalDeviceCount);
+    vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices.data());
+
+    VkPhysicalDeviceProperties physicalDeviceProps;
+    vkGetPhysicalDeviceProperties(physicalDevices[0], &physicalDeviceProps);
+
+    std::cout << physicalDeviceProps.deviceName << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Test();
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
